91d7ae06433c4b62431aa7a149cb505016ecd5bc
chanpark036 <cpark036@gmail.com>
2021-10-23 16:25:20 -0400

    breath

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import MeditationPage from './components/MeditationPage'
+import Breathing from './components/Breathing'
 
 function App() {
   return (
     <div className="App">
       <MeditationPage/>
+      <Breathing/>
     </div>
   );
 }
---
+++ src/components/Breathing.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Breathing extends React.Component {
+    render() { 
+        return <div>Testing</div>;
+    }
+}
+ 
+export default Breathing;
