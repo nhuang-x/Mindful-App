e182d1fc5e95e4ca118472ad67ea2c832250cd5f
Nathan Huang <nathan.huang99@gmail.com>
2021-10-24 12:02:21 -0400

    Updates

---
+++ precache-manifest.9487828d535cfc124aa37b3b5211b9c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e0391b9f37eec8a02d6410ffda021a",
+    "url": "/Mindful-App/index.html"
+  },
+  {
+    "revision": "04bc1c0bc9b500942e37",
+    "url": "/Mindful-App/static/css/main.98216e4c.chunk.css"
+  },
+  {
+    "revision": "f6012a12c1d86fa2f973",
+    "url": "/Mindful-App/static/js/2.6b51a879.chunk.js"
+  },
+  {
+    "revision": "c4d8c5abb040985438207b990a6d2ee6",
+    "url": "/Mindful-App/static/js/2.6b51a879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ef3d6144432dc5a8f9",
+    "url": "/Mindful-App/static/js/3.a6443331.chunk.js"
+  },
+  {
+    "revision": "04bc1c0bc9b500942e37",
+    "url": "/Mindful-App/static/js/main.b7cb9d90.chunk.js"
+  },
+  {
+    "revision": "ed3234bff395d04cdcf7",
+    "url": "/Mindful-App/static/js/runtime-main.35b8cbf2.js"
+  },
+  {
+    "revision": "2121bffef4906bd02aaa59c7a89dd637",
+    "url": "/Mindful-App/static/media/The-science-of-healing-breath-infographic-683x1024.2121bffe.jpeg"
+  },
+  {
+    "revision": "84e644e38ddfc911c44090b6ce905a2c",
+    "url": "/Mindful-App/static/media/breatheinout.84e644e3.gif"
+  },
+  {
+    "revision": "29724496ef1cd645de65d0d1c1f2b693",
+    "url": "/Mindful-App/static/media/square-breathing.29724496.png"
+  }
+]);
