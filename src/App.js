33766513496d65de28144197f99693672ee5b7d7
Ris Ravula <rishi.ravula@duke.edu>
2021-10-23 22:33:52 -0400

    Finished Guided Meditation and Yoga page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import MeditationPage from './components/MeditationPage'
 import Breathing from './components/Breathing'
+import Meditation from './components/GuidedMeditation'
  
 function App() {
  return (
    <div className="App">
      <MeditationPage/>
      <Breathing/>
+     <Meditation/>
    </div>
  );
 }
--- src/components/Breathing.jsx
+++ src/components/Breathing.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import gif from './breatheinout.gif'
 import {Card, CardContent, CardHeader,Grid} from '@material-ui/core'
+
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import BreathingInfo from './BreathingInfo';
@@ -14,24 +15,33 @@ class Breathing extends React.Component {
                     title="Breathing Exercises"
                     subheader="It's Important!!!"
                 />
+                
                 <CardContent>
                     <Grid container >
                         <Grid direction="row" xs>
                             <img src={gif} alt="loading..." />
                             </Grid>
                         <Grid direction="row" xs={4}>
-                            <p>
-                            Follow the blue circle, breath in when the circle expands, hold, breath out when the circle shrinks, hold.
-                            </p>
+                        <p>Follow the Blue Circle</p>
+                           <p>Breath In When the Circle Expands</p>
+                           <p>Breath Out when the Circle Shrinks</p>
+                           <p>Repeat Until Calm</p>
+
+                           
+                            
                         </Grid>
                         <Grid direction="row" xs>
                             <BreathingInfo></BreathingInfo>
                         </Grid>
                     </Grid>
                 </CardContent>
+
+                
             </Card>
 
         </div>
+
+        
     </div>  
    }
 }
